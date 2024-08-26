cbe8e67f50177a3efb5f86d531bf4a58db71dd2e
Aidan Franits <58407173+AidFTech@users.noreply.github.com>
2024-08-25 19:03:13 -0500

    Light Symbols
    Light symbols created with a rudimentary vehicle information window.

--- Navigation Computer/Code/AidF_Nav_Computer_Pi/Vehicle_Information/Vehicle_Info_Window.cpp
+++ Navigation Computer/Code/AidF_Nav_Computer_Pi/Vehicle_Information/Vehicle_Info_Window.cpp
@@ -8,10 +8,50 @@ VehicleInfoWindow::VehicleInfoWindow(AttributeList *attribute_list, InfoParamete
 		title_box->setText("Hybrid Power Flow");
 	else
 		title_box->setText("Vehicle Information");
+
+	this->drl_texture = SDL_CreateTexture(this->renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, 53, 24);
+	SDL_SetTextureBlendMode(this->drl_texture, SDL_BLENDMODE_BLEND);
+	SDL_UpdateTexture(this->drl_texture, NULL, image_data_DRL, 4*53);
+
+	this->side_texture = SDL_CreateTexture(this->renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, 57, 22);
+	SDL_SetTextureBlendMode(this->side_texture, SDL_BLENDMODE_BLEND);
+	SDL_UpdateTexture(this->side_texture, NULL, image_data_SideMarkers, 4*57);
+
+	this->lowbeam_texture = SDL_CreateTexture(this->renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, 55, 30);
+	SDL_SetTextureBlendMode(this->lowbeam_texture, SDL_BLENDMODE_BLEND);
+	SDL_UpdateTexture(this->lowbeam_texture, NULL, image_data_LowBeam, 4*55);
+
+	this->highbeam_texture = SDL_CreateTexture(this->renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, 55, 24);
+	SDL_SetTextureBlendMode(this->highbeam_texture, SDL_BLENDMODE_BLEND);
+	SDL_UpdateTexture(this->highbeam_texture, NULL, image_data_HighBeam, 4*55);
+
+	this->frontfog_texture = SDL_CreateTexture(this->renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, 42, 27);
+	SDL_SetTextureBlendMode(this->frontfog_texture, SDL_BLENDMODE_BLEND);
+	SDL_UpdateTexture(this->frontfog_texture, NULL, image_data_FrontFog, 4*42);
+
+	this->rearfog_texture = SDL_CreateTexture(this->renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, 40, 24);
+	SDL_SetTextureBlendMode(this->rearfog_texture, SDL_BLENDMODE_BLEND);
+	SDL_UpdateTexture(this->rearfog_texture, NULL, image_data_RearFog, 4*40);
 }
 
 VehicleInfoWindow::~VehicleInfoWindow() {
+	if(this->drl_texture != NULL)
+		SDL_DestroyTexture(this->drl_texture);
 
+	if(this->side_texture != NULL)
+		SDL_DestroyTexture(this->side_texture);
+
+	if(this->lowbeam_texture != NULL)
+		SDL_DestroyTexture(this->lowbeam_texture);
+
+	if(this->highbeam_texture != NULL)
+		SDL_DestroyTexture(this->highbeam_texture);
+
+	if(this->frontfog_texture != NULL)
+		SDL_DestroyTexture(this->frontfog_texture);
+
+	if(this->rearfog_texture != NULL)
+		SDL_DestroyTexture(this->rearfog_texture);
 }
 
 void VehicleInfoWindow::refreshWindow() {
@@ -23,4 +63,33 @@ void VehicleInfoWindow::drawWindow() {
 		return;
 	
 	this->title_box->drawText();
+	if(this->drl_texture != NULL) {
+		SDL_Rect drl_rect = {90,120,53,24};
+		SDL_RenderCopy(this->renderer, this->drl_texture, NULL, &drl_rect);
+	}
+
+	if(this->side_texture != NULL) {
+		SDL_Rect side_rect = {150,120,57,22};
+		SDL_RenderCopy(this->renderer, this->side_texture, NULL, &side_rect);
+	}
+
+	if(this->lowbeam_texture != NULL) {
+		SDL_Rect lowbeam_rect = {90, 150, 55, 30};
+		SDL_RenderCopy(this->renderer, this->lowbeam_texture, NULL, &lowbeam_rect);
+	}
+
+	if(this->highbeam_texture != NULL) {
+		SDL_Rect highbeam_rect = {150, 150, 55, 24};
+		SDL_RenderCopy(this->renderer, this->highbeam_texture, NULL, &highbeam_rect);
+	}
+
+	if(this->frontfog_texture != NULL) {
+		SDL_Rect frontfog_rect = {90, 180, 42, 27};
+		SDL_RenderCopy(this->renderer, this->frontfog_texture, NULL, &frontfog_rect);
+	}
+
+	if(this->rearfog_texture != NULL) {
+		SDL_Rect rearfog_rect = {150, 180, 40, 24};
+		SDL_RenderCopy(this->renderer, this->rearfog_texture, NULL, &rearfog_rect);
+	}
 }
--- Navigation Computer/Code/AidF_Nav_Computer_Pi/Vehicle_Information/Vehicle_Info_Window.h
+++ Navigation Computer/Code/AidF_Nav_Computer_Pi/Vehicle_Information/Vehicle_Info_Window.h
@@ -1,10 +1,18 @@
 #include <stdint.h>
 
+#include "../Window/Nav_Window.h"
+
+#include "Vehicle_Info_Parameters.h"
+
 #ifndef vehicle_info_window_h
 #define vehicle_info_window_h
 
-#include "../Window/Nav_Window.h"
-#include "Vehicle_Info_Parameters.h"
+#include "DRL_Img.h"
+#include "SideMarkers_Img.h"
+#include "LowBeam_Img.h"
+#include "HighBeam_Img.h"
+#include "FrontFog_Img.h"
+#include "RearFog_Img.h"
 
 class VehicleInfoWindow : public NavWindow {
 public:
@@ -17,6 +25,13 @@ public:
 private:
     InfoParameters* info_parameters;
     TextBox* title_box;
+
+    SDL_Texture* drl_texture = NULL,
+                *side_texture = NULL,
+                *lowbeam_texture = NULL,
+                *highbeam_texture = NULL,
+                *frontfog_texture = NULL,
+                *rearfog_texture = NULL;
 };
 
 #endif
