458dcdaad7e6b8e0ba68a7b13edd0a4ed7334d64
Aidan Franits <58407173+AidFTech@users.noreply.github.com>
2024-09-02 16:08:02 -0500

    Volume Handler Parameter List
    Added a parameter list to the volume handler.

--- Radio Tuner/Code/AidF_Radio/Volume_Handler.cpp
+++ Radio Tuner/Code/AidF_Radio/Volume_Handler.cpp
@@ -1,7 +1,9 @@
 #include "Volume_Handler.h"
 
-VolumeHandler::VolumeHandler(MCP4251* vol_mcp, MCP4251* treble_mcp, MCP4251* bass_mcp, MCP4251* fader_mcp, AIBusHandler* ai_handler) {
+VolumeHandler::VolumeHandler(MCP4251* vol_mcp, MCP4251* treble_mcp, MCP4251* bass_mcp, MCP4251* fader_mcp, ParameterList* parameters, AIBusHandler* ai_handler) {
 	this->ai_handler = ai_handler;
+	this->parameters = parameters;
+
 	this->vol_mcp = vol_mcp;
 	this->treble_mcp = treble_mcp;
 	this->bass_mcp = bass_mcp;
@@ -23,6 +25,23 @@ VolumeHandler::VolumeHandler(MCP4251* vol_mcp, MCP4251* treble_mcp, MCP4251* bas
 	this->bass_mcp->DigitalPotSetWiperPosition(1, 0);
 }
 
+//Handle an AIBus message.
+bool VolumeHandler::handleAIBus(AIData *msg) {
+	if(msg->receiver == ID_RADIO && msg->sender == ID_NAV_SCREEN) {
+		if(msg->l >= 3 && msg->data[0] == 0x32 && msg->data[1] == 0x6) {
+			uint16_t new_volume = volume;
+			if(msg->data[2]&0x10 != 0)
+				new_volume += msg->data[2]&0xF;
+			else
+				new_volume -= msg->data[2]&0xF;
+			setVolume(new_volume);
+			return true;
+		}
+	}
+
+	return false;
+}
+
 //Set the volume range.
 void VolumeHandler::setVolRange(const uint16_t vol_range) {
 	this->vol_range = vol_range;
@@ -33,33 +52,42 @@ uint16_t VolumeHandler::getVolRange() {
 	return this->vol_range;
 }
 
+//Refresh the volumes.
+void VolumeHandler::setVolume() {
+	setVolume(volume);
+}
+
 //Set the volume.
 void VolumeHandler::setVolume(const uint16_t volume) {
 	this->volume = volume;
 	if(this->volume > this->vol_range)
 		this->volume = this->vol_range;
 
-	uint16_t lf_vol = volume*DEFAULT_RANGE/vol_range, rf_vol = volume*DEFAULT_RANGE/vol_range, lr_vol = volume*DEFAULT_RANGE/vol_range, rr_vol = volume*DEFAULT_RANGE/vol_range;
-	if(balance < 0) { //Left balance.
-		rf_vol = rf_vol*(DEFAULT_RANGE/2 - abs(balance))/(DEFAULT_RANGE/2);
-		rr_vol = rr_vol*(DEFAULT_RANGE/2 - abs(balance))/(DEFAULT_RANGE/2);
-	} else if(balance > 0) { //Right balance.
-		lf_vol = lf_vol*(DEFAULT_RANGE/2 - abs(balance))/(DEFAULT_RANGE/2);
-		lr_vol = lr_vol*(DEFAULT_RANGE/2 - abs(balance))/(DEFAULT_RANGE/2);
-	}
+	if(!this->parameters->digital_amp) { //Analog mode.
+		uint16_t lf_vol = volume*DEFAULT_RANGE/vol_range, rf_vol = volume*DEFAULT_RANGE/vol_range, lr_vol = volume*DEFAULT_RANGE/vol_range, rr_vol = volume*DEFAULT_RANGE/vol_range;
+		if(balance < 0) { //Left balance.
+			rf_vol = rf_vol*(DEFAULT_RANGE/2 - abs(balance))/(DEFAULT_RANGE/2);
+			rr_vol = rr_vol*(DEFAULT_RANGE/2 - abs(balance))/(DEFAULT_RANGE/2);
+		} else if(balance > 0) { //Right balance.
+			lf_vol = lf_vol*(DEFAULT_RANGE/2 - abs(balance))/(DEFAULT_RANGE/2);
+			lr_vol = lr_vol*(DEFAULT_RANGE/2 - abs(balance))/(DEFAULT_RANGE/2);
+		}
 
-	if(fader > 0) { //Front fader.
-		lr_vol = lr_vol*(DEFAULT_RANGE/2 - abs(fader))/(DEFAULT_RANGE/2);
-		rr_vol = rr_vol*(DEFAULT_RANGE/2 - abs(fader))/(DEFAULT_RANGE/2);
-	} else if(fader < 0) { //Rear fader.
-		lf_vol = lf_vol*(DEFAULT_RANGE/2 - abs(fader))/(DEFAULT_RANGE/2);
-		rf_vol = rf_vol*(DEFAULT_RANGE/2 - abs(fader))/(DEFAULT_RANGE/2);
-	}
+		if(fader > 0) { //Front fader.
+			lr_vol = lr_vol*(DEFAULT_RANGE/2 - abs(fader))/(DEFAULT_RANGE/2);
+			rr_vol = rr_vol*(DEFAULT_RANGE/2 - abs(fader))/(DEFAULT_RANGE/2);
+		} else if(fader < 0) { //Rear fader.
+			lf_vol = lf_vol*(DEFAULT_RANGE/2 - abs(fader))/(DEFAULT_RANGE/2);
+			rf_vol = rf_vol*(DEFAULT_RANGE/2 - abs(fader))/(DEFAULT_RANGE/2);
+		}
 
-	this->vol_mcp->DigitalPotSetWiperPosition(0, rf_vol);
-	this->vol_mcp->DigitalPotSetWiperPosition(1, lf_vol);
-	this->fader_mcp->DigitalPotSetWiperPosition(0, rr_vol);
-	this->fader_mcp->DigitalPotSetWiperPosition(1, lr_vol);
+		this->vol_mcp->DigitalPotSetWiperPosition(0, rf_vol);
+		this->vol_mcp->DigitalPotSetWiperPosition(1, lf_vol);
+		this->fader_mcp->DigitalPotSetWiperPosition(0, rr_vol);
+		this->fader_mcp->DigitalPotSetWiperPosition(1, lr_vol);
+	} else { //Digital mode.
+		//TODO: Send a volume change message to the amp.
+	}
 }
 
 //Get the set volume.
